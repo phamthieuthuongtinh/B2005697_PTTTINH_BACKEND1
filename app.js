af10b956a3f9f2e50d5f9b67fd0bb21efa7d8804
phamthieuthuongtinh <tinhb2005697@student.ctu.edu.vn>
2024-01-30 13:35:57 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
 
 const app = express();
 
-const contactsRouter = require("./app/routes/contact.route");
+
 
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.json({message:"Welcome to contact book application."});
-});
+
+
 app.use("/api/contacts", contactsRouter);
 
 // handle 404 response
@@ -24,9 +23,11 @@ app.use((req, res, next) => {
     // define error-handling middleware last, after other app.use() and routes calls
 app.use((err, req, res, next) => {
 
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
     });
 });
-
+app.get("/",(req,res)=>{
+    res.json({message:"Welcome to contact book application."});
+});
 module.exports =app;
--- app/api-error.js
+++ app/api-error.js
@@ -1,9 +1,10 @@
 class ApiError extends Error {
-    constructor(statusCode, message){
-        super();
-        this.statusCode =statusCode;
-        this.message=message;
+    constructor(statusCode, message) {
+      super();
+      this.statusCode = statusCode;
+      this.message = message;
     }
-}
-
-module.exports = ApiError;
+  }
+  
+  module.exports = ApiError;
+  
